1b8c8a6bc2e67a1a2e335d99c4083605894f3f72
Jeff Reback <jeff@reback.net>
2014-12-13 10:03:51 -0500

    BLD: dynd build in separate travis run

--- pandas/util/print_versions.py
+++ pandas/util/print_versions.py
@@ -63,6 +63,8 @@ def show_versions(as_json=False):
         ("nose", lambda mod: mod.__version__),
         ("Cython", lambda mod: mod.__version__),
         ("numpy", lambda mod: mod.version.version),
+        ("dynd", lambda mod: "{dynd} [{libdynd}]".format(dynd=mod.__version__,
+                                                         libdynd=mod.__libdynd_version__)),
         ("scipy", lambda mod: mod.version.version),
         ("statsmodels", lambda mod: mod.__version__),
         ("IPython", lambda mod: mod.__version__),
